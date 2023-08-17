fb5ea58e23e826a93fd871ae929fe70f535b1c3c
Your Name <urjabajj2013@gmail.com>
2023-08-17 17:26:38 +0545

    Add project files.

---
+++ MyAppWeb/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAppWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyAppWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAppWeb.Models;
+
+namespace MyAppWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
