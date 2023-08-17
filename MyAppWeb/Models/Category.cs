5150b03141b2cf6345711fd87c62d663d88d9ab5
Your Name <urjabajj2013@gmail.com>
2023-08-17 22:30:43 +0545

    CRUD done with toastr notification

--- MyAppWeb/Models/Category.cs
+++ MyAppWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyAppWeb.Models
 {
@@ -8,6 +9,7 @@ namespace MyAppWeb.Models
         public int Id { get; set; }
         [Required]      
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder{ get; set; }
         public DateTime CreatedDateTime{ get; set; } = DateTime.Now;
     }
