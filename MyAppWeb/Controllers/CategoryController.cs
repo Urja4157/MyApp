a79dddc90cf187137cee03953007e67ac91f4a1d
Your Name <urjabajj2013@gmail.com>
2023-08-17 21:33:11 +0545

    Create Category

--- MyAppWeb/Controllers/CategoryController.cs
+++ MyAppWeb/Controllers/CategoryController.cs
@@ -1,13 +1,40 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using MyAppWeb.Data;
+using MyAppWeb.Models;
 
 namespace MyAppWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<Category> categories = _context.Categories;
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
         }
     }
 }
