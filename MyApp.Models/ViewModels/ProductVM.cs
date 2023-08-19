ffab680f919e95186784cc13601e6f8074b0e2ca
Your Name <urjabajj2013@gmail.com>
2023-08-19 19:43:25 +0545

    Create New Product

---
+++ MyApp.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
